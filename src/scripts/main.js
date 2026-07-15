6f4cb181fa2a879600c63084c7580f0d6b6066c3
ILLYA SMYK <smykillya@gmail.com>
2026-07-15 21:50:39 +0200

    add task solution3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector('.contacts__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+
+    return;
+  }
+
+  form.reset();
+});
